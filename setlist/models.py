5d16e10b23f773b49bb907ceff2d1cc154c18be3
mjjstockman <mjjstockman@gmail.com>
2022-06-21 13:19:26 +0000

    Add models to setlist app

--- setlist/models.py
+++ setlist/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, 'Waiting Confirmation'), (1, 'Published'))
+
+
+class Venue(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Release(models.Model):
+    title = models.CharField(max_length=200)
+    songs = models.ManyToManyField(Song, related_name="releases")
+    slug = models.SlugField(max_length=200, unique=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Gig(models.Model):
+    date = models.DateField(auto_now_add=False, auto_now=False, blank=True,
+                            null=True)
+    venue = models.ForeignKey('setlist.Venue', on_delete=models.CASCADE,
+                              null=True)
+    featured_image = CloudinaryField('image', null=True, blank=True)
+    featured_image_status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return f"{self.venue} in {self.venue.city} on {self.date}"
+
+
+class Setlist(models.Model):
+    gig = models.OneToOneField('Gig', related_name='setlist_gig',
+                               on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    song = models.ManyToManyField(Song)
+    status = models.IntegerField(choices=STATUS, default=0)
+    agree = models.ManyToManyField(User, related_name='setlist_agree',
+                                   blank=True)
+    disagree = models.ManyToManyField(User, related_name='setlist_disagree',
+                                      blank=True)
+
+    # class Meta:
+    #     ordering = ['gig.date']
+
+    def __str__(self):
+        return f"{self.gig.venue} on {self.gig.date}"
