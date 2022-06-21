0eaa0a7c61c6fade5c7ad668538f4bf75dce6db7
mjjstockman <mjjstockman@gmail.com>
2022-06-21 13:39:06 +0000

    Add models to admin

--- setlist/admin.py
+++ setlist/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Venue, Song, Release, Gig, Setlist
 
-# Register your models here.
+admin.site.register(Venue)
+admin.site.register(Song)
+admin.site.register(Release)
+admin.site.register(Gig)
+admin.site.register(Setlist)
