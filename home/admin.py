67a6def0408117249781051b10b2056e87ccdeac
mjjstockman <mjjstockman@gmail.com>
2022-06-23 13:04:38 +0000

    Update gig view, url and admin registration

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Gig
 
 # Register your models here.
+
+
+admin.site.register(Gig)
--- home/views.py
+++ home/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
 from .models import Gig
 
-# Create your views here.
-# def index(request):
-#     return render(request, 'index.html')
 
 def gigs(request):
-    # setlists = Setlist.objects.filter(status=1)
     gigs = Gig.objects.all()
     context = {
-        # 'setlists': setlists,
         'gigs': gigs,
     }
     return render(request, 'home/index.html', context)
