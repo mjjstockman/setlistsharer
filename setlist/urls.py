ed932f30b872d1bf4541b02e47a4feee1f97977f
mjjstockman <mjjstockman@gmail.com>
2022-06-24 16:23:21 +0000

    Add edit url to setlist/urls

--- setlist/urls.py
+++ setlist/urls.py
@@ -5,6 +5,6 @@ from .views import add
 urlpatterns = [
     # path('', get_setlists, name='setlist'),
     path('add/<str:pk>/', add, name='add_setlist'),
-    # path('edit/<str:pk>/', edit, name='edit_setlist'),
+    path('edit/<str:pk>/', edit, name='edit_setlist'),
     # path('delete/<str:pk>/', delete, name='delete_setlist'),
 ]
