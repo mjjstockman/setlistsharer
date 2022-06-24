1bbf521fe4723cd2656f0f5a5ec74ea5d052f99b
mjjstockman <mjjstockman@gmail.com>
2022-06-24 16:20:40 +0000

    Add edit function to setlist/views

--- setlist/views.py
+++ setlist/views.py
@@ -5,7 +5,7 @@ from .forms import SetlistForm
 from home.views import Gig
 
 
-def add(request, pk):
+def add(request, pk):  
     gig = Gig.objects.get(id=pk)
     author = request.user
     initial = {
@@ -24,3 +24,19 @@ def add(request, pk):
         'form': form
     }
     return render(request, 'add.html', context)
+
+
+def edit(request, pk):
+    setlist = Setlist.objects.get(id=pk)
+    form = SetlistForm(instance=setlist)
+
+    if request.method == 'POST':
+        form = SetlistForm(request.POST, instance=setlist)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'setlist/add.html', context)
