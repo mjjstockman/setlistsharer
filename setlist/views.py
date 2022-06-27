c19b136f9d2c4c52eb097f9f36d9f330d8f2818b
mjjstockman <mjjstockman@gmail.com>
2022-06-27 11:22:20 +0000

    Remove dulpicate edit finction in setlist/views

--- setlist/views.py
+++ setlist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from home.views import Gig
 from .forms import SetlistAddForm, SetlistEditForm
 from .models import Setlist
 from home.views import Gig
@@ -39,18 +40,5 @@ def edit(request, pk):
 
     context = {
         'form': form,
-    }
-    return render(request, 'add.html', context)def edit(request, pk):
-    setlist = Setlist.objects.get(id=pk)
-    form = SetlistEditForm(instance=setlist)
-
-    if request.method == 'POST':
-        form = SetlistEditForm(request.POST, instance=setlist)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
-
-    context = {
-        'form': form,
-    }
+    }    
     return render(request, 'add.html', context)
