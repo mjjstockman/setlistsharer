6d2486acba64d9ef2a4eee5885b626112498c0e4
mjjstockman <mjjstockman@gmail.com>
2022-06-28 18:19:58 +0000

    Add get_object_or_404 to setlist/views

--- setlist/views.py
+++ setlist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
