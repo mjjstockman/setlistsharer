e9e840fece6541932dc7b1c05f399f6f1f40601a
mjjstockman <mjjstockman@gmail.com>
2022-07-06 14:13:14 +0000

    Update home tests- NOT WORKING

--- home/tests.py
+++ home/tests.py
@@ -1,12 +1,22 @@
 from django.test import TestCase
-
+from django.test import Client
+from django.urls import reverse
+from .models import Gig
 # Create your tests here.
 
 
 class TestViews(TestCase):
 
-    def test_home_page(self):
-        response = self.client.get('/')
+    def setUp(self):
+        self.client = Client()
+        testgig = Gig.objects.create()
+
+        
+    def testhome(self):
+        response = self.client.get(reverse('index'))
+        print(response.content)
+        print(response.headers)
+        print(response.reason_phrase)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
         self.assertTemplateUsed(response, 'base.html')
