af7693b4d471a5430f96ad6b2b66ebde20b654e7
mjjstockman <mjjstockman@gmail.com>
2022-07-06 11:07:57 +0000

    Show random gig image for gigs without a published image

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from setlist.models import Setlist
 from .models import Gig
@@ -7,11 +8,13 @@ from .models import Gig
 
 def gigs(request):
     gigs = Gig.objects.all().order_by('date')
+    random_num = random.randrange(0, 14)
     
 
 
     context = {
         'gigs': gigs,
+        'random_num': random_num
     }
     return render(request, 'home/index.html', context)
 
