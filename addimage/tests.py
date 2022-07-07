a4ce49dd318468bcdb6c59677f67021293781e52
mjjstockman <mjjstockman@gmail.com>
2022-07-07 10:44:01 +0000

    Add addimage url test

--- addimage/tests.py
+++ addimage/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from datetime import datetime
+# from django.test import SimpleTestCase, TestCase, Client
+# from django.core.urlresolvers import reverse
+from django.test import Client
+from django.urls import reverse, resolve
+from django.test import SimpleTestCase
+from .views import add_image
 
 # Create your tests here.
+
+
+class TestUrls(SimpleTestCase):
+          
+    def test_addimage_url(self):
+        url = reverse('add-image', args=[1])
+        self.assertEqual(resolve(url).func, add_image)
