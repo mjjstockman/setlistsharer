8a7b09ddc727b1da7b4e592138f1272d2ee0c728
mjjstockman <mjjstockman@gmail.com>
2022-07-08 14:28:55 +0000

    Begin creating objects for tests

--- setlist/tests.py
+++ setlist/tests.py
@@ -3,47 +3,76 @@ from datetime import datetime
 from django.test import Client
 from django.urls import reverse, resolve
 from django.test import SimpleTestCase
+from django.contrib.auth.models import User
+from home.models import Gig
 from .views import add, edit, detail, delete, agree, disagree
-from .models import Venue, Song, Release
+from .models import Venue, Song, Release, Setlist
 
-
-class TestUrls(TestCase):   
-
-    def test_add(self):
-        url = reverse('add_setlist', args=[1])
-        self.assertEqual(resolve(url).func, add)
-        
-    def test_edit(self):
-        url = reverse('edit_setlist', args=[1])
-        self.assertEqual(resolve(url).func, edit)
-  
-    def test_detail(self):
-        url = reverse('delete_setlist', args=[1])
-        self.assertEqual(resolve(url).func, delete)
-
-    def test_agree(self):
-        url = reverse('agree', args=[1])
-        self.assertEqual(resolve(url).func, agree)
-
-    def test_disagree(self):
-        url = reverse('disagree', args=[1])
-        self.assertEqual(resolve(url).func, disagree)
 
 
 class TestViews(TestCase):
 
     def test_setlist_detail(self):
 
-        Venue.objects.create(
-            name = 'A Venue',
-            city = 'A City',
+        venue=Venue.objects.create(
+            name='A Venue',
+            city='A City',
         )
 
-        song1 = Song.objects.create(
-            title = 'A Title',
+        song=Song.objects.create(
+            title='A Title',
         )
        
-        Release.objects.create(
-            title = 'An Album',
-            songs = song1
+        release=Release.objects.create(
+            title='An Album',
         )
+
+        setlist=Setlist.objects.create(
+            status=0,
+        )
+
+        gig=Gig.objects.create(
+            date=datetime.now()
+        )
+
+        
+        user=User.objects.create(
+            username='user',
+            password='dphgfsS34',
+            email='c@c.com',
+        )
+
+        # song.save()
+        # gig.save()
+        # venue.save()
+        # release.save()
+        # setlist.save()
+        # user.save()
+
+        release.songs.add(song)
+        setlist.gig.add(gig)
+      
+      
+
+
+    #   class TestUrls(TestCase):   
+
+    # def test_add(self):
+    #     url = reverse('add_setlist', args=[1])
+    #     self.assertEqual(resolve(url).func, add)
+        
+    # def test_edit(self):
+    #     url = reverse('edit_setlist', args=[1])
+    #     self.assertEqual(resolve(url).func, edit)
+  
+    # def test_detail(self):
+    #     url = reverse('delete_setlist', args=[1])
+    #     self.assertEqual(resolve(url).func, delete)
+
+    # def test_agree(self):
+    #     url = reverse('agree', args=[1])
+    #     self.assertEqual(resolve(url).func, agree)
+
+    # def test_disagree(self):
+    #     url = reverse('disagree', args=[1])
+    #     self.assertEqual(resolve(url).func, disagree)
