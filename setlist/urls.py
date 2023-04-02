45db5cb3626a5b9827992cc9b95835dc781ae4f7
mjjstockman <mjjstockman@gmail.com>
2023-04-02 16:00:54 +0000

    Clean setlist/views

--- setlist/urls.py
+++ setlist/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from addimage.views import add_image
-from .views import add, edit, detail, delete, agree, like
+from .views import add, edit, detail, delete, like
 from . import views
 
 
@@ -9,10 +9,6 @@ urlpatterns = [
     path('edit/<str:pk>/', edit, name='edit_setlist'),
     path('detail/<str:pk>/', detail, name='detail_setlist'),
     path('delete/<str:pk>/', delete, name='delete_setlist'),
-    # path('agree/<str:pk>/', agree, name='agree'),
-    # path('like/<str:pk>/', like, name='like'),
-    # path('agree/<str:pk>', views.SetlistAgree.as_view(), name='setlist_agree'),
-    # path('disagree/<str:pk>/', disagree, name='disagree'),
     path('like/<str:pk>/', like, name='like'),
     path('add-image/<str:pk>/', add_image, name='add_image'),
 ]
