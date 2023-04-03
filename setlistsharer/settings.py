63af4a0ed83346f1518b577fed860a81b6ebc539
mjjstockman <mjjstockman@gmail.com>
2023-04-03 14:08:04 +0000

    Remove clousinary.uploader and cloudinary.api from settings

--- setlistsharer/settings.py
+++ setlistsharer/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 import dj_database_url
 if os.path.isfile('env.py'):
