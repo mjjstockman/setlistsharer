3f714b93807323c90a05f6e2638144a1b4d23034
mjjstockman <mjjstockman@gmail.com>
2023-04-05 13:40:05 +0000

    Remove unused css

--- home/models.py
+++ home/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 STATUS = ((0, 'No Image'), (1, 'Waiting Confirmation'), (2, 'Published'))
 
 
